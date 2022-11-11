433604ca60bf47f638934577fd8a563ecd5fc623
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2022-11-11 04:42:41 -0600

    Add header for form

--- src/components/dep/AddNewBook.js
+++ src/components/dep/AddNewBook.js
@@ -4,16 +4,19 @@ import './addNewBook.css';
 function AddNewBook() {
   return (
     <div>
-      <section className="books-authors-inputs">
-        <input
-          placeholder="Book Title"
-          type="text"
-        />
-        <input
-          placeholder="Author"
-          type="text"
-        />
-        <button className="add-books" type="button">Add Book</button>
+      <section className="books-authors-inputs-div">
+        <h2>Add New Book</h2>
+        <form className="books-authors-inputs">
+          <input
+            placeholder="Book Title"
+            type="text"
+          />
+          <input
+            placeholder="Author"
+            type="text"
+          />
+          <button className="add-books" type="button">Add Book</button>
+        </form>
       </section>
     </div>
   );
