03731fd27c282c8b3a7a5afe0d6b9bef243ddf67
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2022-11-10 19:10:40 -0600

    Add navigation and route to pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import Home from './components/pages/Home';
+import Books from './components/pages/Books';
+import Categories from './components/pages/Categories';
 import './App.css';
 
-function App() {
-  return (
+const App = () => (
+  <Router>
     <div className="App">
-      <h1>BookKeeper</h1>
+      <Navigation />
+      <div className="container">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/books" element={<Books />} />
+          <Route exact path="/categories" element={<Categories />} />
+        </Routes>
+      </div>
     </div>
-  );
-}
+  </Router>
+);
 
 export default App;
