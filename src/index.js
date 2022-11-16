b129c7cf0b9cbaf9b0029ff71d95b3acdaeea6e8
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2022-11-16 07:10:21 -0600

    Fix spacing error

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
   </React.StrictMode>,
 );
