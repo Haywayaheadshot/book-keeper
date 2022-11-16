e02f12f9f3f8c08305b89672cd657a31b9826c76
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2022-11-16 10:02:07 -0600

    Add genre,currentStatus and progress prop and import RemoveBook module

--- src/components/dep/Book.js
+++ src/components/dep/Book.js
@@ -1,25 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import '../../styles/book.css';
+import RemoveBook from './RemoveBook';
 
-const Book = ({ title, author }) => (
+const Book = ({
+  id, title, author, progress, genre, currentStatus,
+}) => (
   <ul className="new-book-entry-ul">
     <li className="new-book-entry">
       <section>
-        <h4>Genre-Holder</h4>
+        <h4>{genre}</h4>
         <h2>{title}</h2>
         <h5>{author}</h5>
         <div className="book-buttons-div">
           <button type="button">Comments</button>
-          <button type="button">Remove</button>
+          <RemoveBook id={id} />
           <button type="button">Edit</button>
         </div>
       </section>
       <section>
         <h5>Progress</h5>
+        <h4>{progress}</h4>
       </section>
       <section>
-        <h5>Current Status</h5>
+        <h5>Current Chapter</h5>
+        <h4>
+          Chapter
+          {currentStatus}
+        </h4>
       </section>
     </li>
   </ul>
@@ -29,4 +37,8 @@ export default Book;
 Book.propTypes = {
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  progress: PropTypes.string.isRequired,
+  currentStatus: PropTypes.string.isRequired,
+  genre: PropTypes.string.isRequired,
 };
