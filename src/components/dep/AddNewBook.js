8936db9c81acd2e50449e5b6134c66af36425193
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2022-11-16 10:03:18 -0600

    Write state handlers for inputs

--- src/components/dep/AddNewBook.js
+++ src/components/dep/AddNewBook.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { v4 as uuid } from 'uuid';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../../redux/books/Books';
 import '../../styles/addNewBook.css';
 
 function AddNewBook() {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [genre, setGenre] = useState('');
+  const dispatch = useDispatch();
+  const uId = uuid();
+  const id = uId.slice(0, 8);
+  const progress = Math.floor(Math.random() * 100);
+  const currentStatus = Math.floor(Math.random() * 10);
+
+  const onChangeTitleHandler = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const onChangeAuthorHandler = (event) => {
+    setAuthor(event.target.value);
+  };
+
+  const onChangeGenreHandler = (event) => {
+    setGenre(event.target.value);
+  };
+
+  const onClickInput = () => {
+    dispatch(addBook({
+      id, title, genre, author, progress: `${progress}%`, currentStatus: `${currentStatus}`,
+    }));
+    setAuthor('');
+    setTitle('');
+    setGenre('');
+  };
+
   return (
     <div>
       <section className="books-authors-inputs-div">
@@ -11,19 +44,24 @@ function AddNewBook() {
             placeholder="Book Title"
             type="text"
             className="user-inputs"
+            value={title}
+            onChange={onChangeTitleHandler}
           />
           <input
             placeholder="Author"
             type="text"
             className="user-inputs"
+            value={author}
+            onChange={onChangeAuthorHandler}
           />
-          <select placeholder="Category">
-            <option>-</option>
-            <option>Suspense</option>
-            <option>Action</option>
-            <option>Romance</option>
-          </select>
-          <button className="add-books" type="button">Add Book</button>
+          <input
+            placeholder="Genre"
+            className="user-inputs"
+            type="text"
+            value={genre}
+            onChange={onChangeGenreHandler}
+          />
+          <button className="add-books" type="button" onClick={onClickInput}>Add Book</button>
         </form>
       </section>
     </div>
