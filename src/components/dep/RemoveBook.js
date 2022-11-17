d991e662bff670c79fd45ed5e36f18e139911ccb
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2022-11-17 14:45:11 -0600

    Refactor to delete book from API

--- src/components/dep/RemoveBook.js
+++ src/components/dep/RemoveBook.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { removeBook } from '../../redux/books/Books';
+import { getBooks, removeBook } from '../../redux/books/Books';
 
 const RemoveBook = (id) => {
   const dispatch = useDispatch();
+  const index = id;
+  const convertedId = index.id;
   const onClickRemove = () => {
-    dispatch(removeBook(id));
+    dispatch(removeBook(convertedId));
+    setTimeout(() => dispatch(getBooks()), 100);
   };
   return (
-    <button type="button" onClick={onClickRemove}>Remove</button>
+    <button className="comment-edit-remove-btn" type="button" onClick={onClickRemove} id={convertedId}>
+      Remove
+    </button>
   );
 };
 
