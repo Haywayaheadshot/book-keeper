ff3f1e623fa5f2e8f156d705e75e3ca4ef72d52f
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2022-11-18 06:07:16 -0600

    Add font family class to logo and nav buttons

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -11,18 +11,18 @@ function Navigation() {
     <div className="nav-bar">
       <section className="heading-section">
         <h1>
-          <NavLink className="header-logo" to="/">
+          <NavLink className="header-logo montserrat-fam" to="/">
             BookStore CMS
           </NavLink>
         </h1>
         <ul className="nav-desk-ul">
           <li>
-            <NavLink className="desk-li" to="/books">
+            <NavLink className="desk-li montserrat-fam" to="/books">
               BOOKS
             </NavLink>
           </li>
           <li>
-            <NavLink className="desk-li" to="/categories">
+            <NavLink className="desk-li montserrat-fam" to="/categories">
               CATEGORIES
             </NavLink>
           </li>
