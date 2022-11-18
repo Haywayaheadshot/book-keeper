7ae8c8b1bf936a57b5d1aaf96441a41b7b247169
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2022-11-18 06:00:17 -0600

    Add respective font-family classes to text and buttons to match zeplin design

--- src/components/dep/Book.js
+++ src/components/dep/Book.js
@@ -11,35 +11,35 @@ const Book = ({
   <ul className="new-book-entry-ul">
     <li className="new-book-entry">
       <section>
-        <h4 className="genre-text">{genre}</h4>
-        <h2 className="title-text">{title}</h2>
-        <h5 className="author-text">{author}</h5>
+        <h4 className="genre-text montserrat-fam">{genre}</h4>
+        <h2 className="title-text robotoslab-fam">{title}</h2>
+        <h5 className="author-text robotoslab-fam">{author}</h5>
         <div className="book-buttons-div">
-          <button className="comment-edit-remove-btn" type="button">Comments</button>
+          <button className="comment-edit-remove-btn robotoslab-fam" type="button">Comments</button>
           <RemoveBook id={id} />
-          <button className="comment-edit-remove-btn" type="button">Edit</button>
+          <button className="comment-edit-remove-btn robotoslab-fam" type="button">Edit</button>
         </div>
       </section>
       <section className="progress-section">
         <h5 className="for-phone">Progress</h5>
         <CircularProgressbar className="progress-bar" value={progress} text={`${progress}%`} />
         <span className="progress-percentage-div">
-          <h4 className="progress-percentage">
+          <h4 className="progress-percentage montserrat-fam">
             {progress}
             %
           </h4>
-          <h6 className="progress-percentage-text">Completed</h6>
+          <h6 className="progress-percentage-text montserrat-fam">Completed</h6>
         </span>
       </section>
       <section className="current-chapter-section">
         <span>
-          <h5 className="current-chap-text">Current Chapter</h5>
-          <h4 className="current-chap-text">
+          <h5 className="current-chap-text robotoslab-fam">Current Chapter</h5>
+          <h4 className="current-chap-text robotoslab-fam">
             Chapter
             {currentStatus}
           </h4>
         </span>
-        <button className="update-status-button" type="button">UPDATE STATUS</button>
+        <button className="update-status-button robotoslab-fam" type="button">UPDATE STATUS</button>
       </section>
     </li>
   </ul>
